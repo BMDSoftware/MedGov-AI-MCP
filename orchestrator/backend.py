3a40cd3c043a4819586fd6dd9781cdf7c23e26d0
GCapaross <gvds@ua.pt>
2026-03-05 13:15:19 +0000

    CI and CD pipeline, containerization

--- orchestrator/backend.py
+++ orchestrator/backend.py
@@ -96,7 +96,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost", "http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
